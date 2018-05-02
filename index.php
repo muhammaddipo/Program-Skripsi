12184bccb025821433428ec18fe648937ba45a35
Teuku Hashrul <hasrultk98@gmail.com>
2018-05-02 21:44:01 +0700

    remove dummy

--- index.php
+++ index.php
@@ -7,12 +7,13 @@
 		<link rel="stylesheet" href="lib/w3.css">
 		<link rel="stylesheet" href="lib/w3-theme-riverside.css">
 		<link rel="stylesheet" href="style/index_style.css">
+		<!-- <link rel="stylesheet" href="fonts/listOfFonts.css"> -->
 	</head>
 	<body>
 		<!-- CONTENT -->
 		<div class="container">
 			<img src="img/banner.jpg" alt="" style="width:100%;">
-			<div class="centered">E library</div>
+			<div class="centered"><h1>E - LIBRARY</h1></div>
 		</div>
 		<a  href="">SIGN UP </a>
 		<a href=''>LOG IN </a>
