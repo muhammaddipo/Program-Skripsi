add76469eb04c8a27140961eeb630233fbd074f5
muhammaddipo <muhammmaddipo@gmail.com>
2020-04-13 13:07:54 +0700

    modif update user

--- model/updateUser.php
+++ model/updateUser.php
@@ -1,81 +1,84 @@
 <?php
-    include 'db.php';
     include 'userClass.php';
-    session_start();
-
+    include 'libraries.php';
     // BIKIN CONVENTION SENDIRI
     //
-    
-    $count = 0 ;
 
     if(isset($_POST['submit'])){
         // if(!$_POST['password'] && !$_POST['confirm_Password']&& !$_POST['phone']&& !$_POST['address']){
         //         header("Location: ../pages/general/profile.php");
         // }
+        $user=$_SESSION['user']->getId();
+        $password=$_POST['password'];
+        $confirm_password=$_POST['confirm_Password'];
+        $phone=$_POST['phone'];
+        $address=$_POST['address'];
+
+        updateUser($user,$password,$confirm_password,$phone,$address);
+    }
+    function updateUser($user,$password,$confirm_password,$phone,$address){
+        include 'libraries.php';
+        $count = 0 ;
         $sql = "UPDATE anggota SET ";
         $pass = false; //buat cek ternyata mau update 
-      
         //cek password
-       if($_POST['password']){
-            
-            if(!$_POST['confirm_Password']){
-                header("Location: ../pages/general/profile.php?statusUpdate=1"); // 1: dua duanya ga diisi
+        if($password){
+                
+                if(!$confirm_password){
+                    header("Location: ../pages/general/profile.php?statusUpdate=1"); // 1: dua duanya ga diisi
+                }
+        }
+        //cek confirm password
+        if($confirm_password){
+            if(!$_POST['password']){
+                header("Location: ../pages/general/profile.php?statusUpdate=1"); //1: salah satu dari password dan confirm password ga diisi 
             }
-       }
-       //cek confirm password
-       if($_POST['confirm_Password']){
-           if(!$_POST['password']){
-               header("Location: ../pages/general/profile.php?statusUpdate=1"); //1: salah satu dari password dan confirm password ga diisi 
-           }
-           
-           if($_POST['confirm_Password'] != $_POST['password']){
-                header("Location: ../pages/general/profile.php?statusUpdate=2"); //2: password sama confirmed password ga sama               
-           }else{
-                $sql.= "password=md5('$_POST[password]') ";
-                $pass = true;
-                $count++;
-           }
-       }
-       // cek phone
-       if($_POST['phone']){
-           if($count > 0){
-             $sql.= ", phone= $_POST[phone] ";
-            $count++;
-             
-           }else{
-            $sql.= " phone= $_POST[phone] ";
-            $count++;
-               
-           }
-       }
-       //cek address
-       if($_POST['address']){
-        if($count > 0){
-            $sql.= ", address= '$_POST[address]' ";
-            $count++;
-          }else{
-           $sql.= " address= '$_POST[address]' ";
-           $count++;
-              
-          }
-       }
-
-       $sql.="WHERE id_Anggota=".$_SESSION['user']->getId();
-       echo "$sql";
-       $result = $mysqli->query($sql);
-       if($result){
-           //statusUpdate = 3 artinya berhasil update selain update password
-           //statusUpdate = 4 artinya berhasil update password juga 
-           if($pass){
-            header("Location: ../pages/general/profile.php?statusUpdate=4");
             
-           }else{
-               header("Location: ../pages/general/profile.php?statusUpdate=3");
+            if($confirm_password != $password){
+                    header("Location: ../pages/general/profile.php?statusUpdate=2"); //2: password sama confirmed password ga sama               
+            }else{
+                    $sql.= "password=md5('$password') ";
+                    $pass = true;
+                    $count++;
+            }
+        }
+        // cek phone
+        if($phone){
+            if($count > 0){
+                $sql.= ", phone= '$phone' ";
+                $count++;
+                
+            }else{
+                $sql.= " phone= '$phone' ";
+                $count++;
+                
+            }
+        }
+        //cek address
+        if($address){
+            if($count > 0){
+                $sql.= ", address= '$address' ";
+                $count++;
+            }else{
+            $sql.= " address= '$address' ";
+            $count++;
+                
+            }
+        }
 
-           }
-       }
-       
+        $sql.="WHERE id_Anggota=".$user;
+        echo "$sql";
+        $result = $mysqli->query($sql);
+        if($result){
+            //statusUpdate = 3 artinya berhasil update selain update password
+            //statusUpdate = 4 artinya berhasil update password juga 
+            if($pass){
+                header("Location: ../pages/general/profile.php?statusUpdate=4");
+                
+            }else{
+                header("Location: ../pages/general/profile.php?statusUpdate=3");
 
-
+            }
+        }
     }
 ?>
